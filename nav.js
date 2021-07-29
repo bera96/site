4fe54f6904046c6b8a3363bc78622d3448584c7a
Bera Yilmazyurt <sentinelprime33@gmail.com>
2021-07-29 11:52:26 +0300

    Coursedetails Bar eklendi.

--- nav.js
+++ nav.js
@@ -1,11 +1,18 @@
   
 
+// Nav elements
 
 const navToggle = document.querySelector(".nav-toggle");
 const links = document.querySelector("#wrapper-nav-ul");
 
+// Tab elements
+
+const btns=document.querySelectorAll(".tab-btn");
+const articles=document.querySelectorAll(".cd-content");
+const courseDetails=document.querySelector("#course-details-article");
 
 
+// NavBar 
 navToggle.addEventListener("click", function () {
   links.classList.toggle("show-links");
   var visibility = links.style.visibility;
@@ -16,3 +23,31 @@ navToggle.addEventListener("click", function () {
  }
   
 });
+
+// Tab
+
+courseDetails.addEventListener("click",function(e){
+  const id=e.target.dataset.id;
+  
+ 
+  if(id){
+    
+    // remove active from other buttons
+
+    btns.forEach(function(btn){
+      btn.classList.remove("active");
+      e.target.classList.add("active");
+    })
+
+    // hide other articles
+
+   articles.forEach(function(article){
+     article.classList.remove("active");
+   }) 
+   const element=document.getElementById(id);
+   element.classList.add("active");
+    
+
+  }
+  
+})
